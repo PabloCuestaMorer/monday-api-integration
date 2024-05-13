7539b2bf6094b1b9929db0ab6a479a6da1d20aec
pcuestamorer <pcuestamorer@gmail.com>
2024-05-13 20:55:56 +0200

    Fix login and "almost" print users

--- TechHomeCare/Main.py
+++ TechHomeCare/Main.py
@@ -1,79 +1,59 @@
-from MondayAPI import fetch_and_format_items
-from flask import Flask, jsonify, make_response, redirect, render_template, request, url_for
-from MondayAPI import fetch_and_format_items
-import json
+from flask import (
+    Flask,
+    jsonify,
+    make_response,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
+from MondayAPI import fetch_items
 
 app = Flask(__name__)
 
-def convertir_meses(data):
-    index = json.loads(data)['index']
-    if index == 5:
-        return '0 meses'
-    elif index == 1:
-        return '3 meses'
-    elif index == 0:
-        return '6 meses'
-    elif index == 2:
-        return '12 meses'
-    else:
-        return 'Desconocido'
+BOARD_ID = 1496686926
 
 
 def verificar_credenciales(nombre_usuario, password):
-    for usuario in fetch_and_format_items(1496686926):
-        if usuario['name'] == nombre_usuario and usuario['Contraseña'] == password:
+    for usuario in fetch_items(BOARD_ID):
+        if usuario["name"] == nombre_usuario and usuario["Contraseña"] == password:
             return True
     return False
 
-@app.route('/login', methods=['POST'])
+
+@app.route("/login", methods=["POST"])
 def login():
-    nombre_usuario = request.form['nombre_usuario']
-    password = request.form['password']
+    nombre_usuario = request.form["nombre_usuario"]
+    password = request.form["password"]
 
     print("el usuario recibido del form es: " + str(nombre_usuario))
 
     if verificar_credenciales(nombre_usuario, password):
         # Credenciales válidas, crear una cookie de usuario autenticado y redirigir a la página principal
-        respuesta = make_response(redirect(url_for('index')))
-        respuesta.set_cookie('usuario_autenticado', nombre_usuario)
+        respuesta = make_response(redirect(url_for("index")))
+        respuesta.set_cookie("usuario_autenticado", nombre_usuario)
         return respuesta
     else:
         # Credenciales inválidas, devolver un mensaje de error
-        return jsonify({'error': 'Nombre de usuario o contraseña incorrectos'}), 401
+        return jsonify({"error": "Nombre de usuario o contraseña incorrectos"}), 401
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    if 'usuario_autenticado' in request.cookies:
+    if "usuario_autenticado" in request.cookies:
         # El usuario está autenticado, mostrar los datos de los usuarios
-        board_id = 1496686926  # Reemplaza con tu ID de tabla real
-        user_data = fetch_and_format_items(board_id)
-        # Procesar los datos para convertir las cadenas JSON en objetos Python
-        for user in user_data:
-            if 'Subcripcion' in user:
-                subcripcion_data = json.loads(user['Subcripcion'])
-                if 'index' in subcripcion_data:
-                    if subcripcion_data['index'] == 1:
-                        subcripcion_data['estado'] = 'alta'
-                    elif subcripcion_data['index'] == 2:
-                        subcripcion_data['estado'] = 'baja'
-                    user['Subcripcion'] = subcripcion_data
-            if 'Telefono' in user:
-                user['Telefono'] = json.loads(user['Telefono'])
-            if 'meses' in user:
-                user['meses'] = convertir_meses(user['meses'])
+        user_data = fetch_items(BOARD_ID)
 
-        return render_template('index.html', users=user_data)
+        return render_template("index.html", users=user_data)
     else:
         # El usuario no está autenticado, redirigir al formulario de inicio de sesión
-        return redirect(url_for('login_form'))
-    
+        return redirect(url_for("login_form"))
 
-@app.route('/login_form')
+
+@app.route("/login_form")
 def login_form():
-    return render_template('login.html')
+    return render_template("login.html")
 
-if __name__ == '__main__':
-    #print(fetch_and_format_items(1496686926))
+
+if __name__ == "__main__":
     app.run(debug=True)
-
-#board_id = 1496686926  # Replace with your actual board ID
--- TechHomeCare/MondayAPI.py
+++ TechHomeCare/MondayAPI.py
@@ -37,45 +37,21 @@ def fetch_board_details(board_id):
             }
         }
     """
-    variables = json.dumps({'boardIds': [str(board_id)]})
-    data = {'query': query, 'variables': variables}
+    variables = json.dumps({"boardIds": [str(board_id)]})
+    data = {"query": query, "variables": variables}
     response = requests.post(url=api_url, headers=headers, json=data)
     return response.json()
 
 
-def format_item_value(column_type, settings_str, value):
-    """Formats an item value based on the column type."""
-    if value is None:
-        return ""
-
-    settings = json.loads(settings_str or "{}")
-
-    if column_type == "text" or column_type == "long-text":
-        return value.strip()
-    elif column_type == "numeric":
-        return value
-    elif column_type == "date":
-        return json.loads(value)["date"]
-    elif column_type == "color":
-        labels = settings["labels"]
-        return labels[str(json.loads(value)["index"])]
-    elif column_type == "dropdown":
-        labels = settings["labels"]
-        label_map = {row["id"]: row["name"] for row in labels}
-        return ", ".join([label_map[id] for id in json.loads(value)["ids"]])
-    else:
-        return value  # Default formatter
-
-
-def fetch_and_format_items(board_id):
+def fetch_items(board_id):
     """Fetches and formats all items from a specified board using items_page for pagination."""
     board_data = fetch_board_details(board_id)
-    if 'errors' in board_data:
-        print("Error fetching board details:", board_data['errors'])
+    if "errors" in board_data:
+        print("Error fetching board details:", board_data["errors"])
         return []
 
-    col_defs = {col['id']: col for col in board_data['data']['boards'][0]['columns']}
-    
+    col_defs = {col["id"]: col for col in board_data["data"]["boards"][0]["columns"]}
+
     query = """
         query ($boardId: ID!) {
             boards (ids: [$boardId]) {
@@ -93,27 +69,22 @@ def fetch_and_format_items(board_id):
             }
         }
     """
-    variables = json.dumps({'boardId': str(board_id)})
-    data = {'query': query, 'variables': variables}
+    variables = json.dumps({"boardId": str(board_id)})
+    data = {"query": query, "variables": variables}
     response = requests.post(url=api_url, headers=headers, json=data)
     items_data = response.json()
-    if 'errors' in items_data:
-        print("Error fetching items:", items_data['errors'])
+    if "errors" in items_data:
+        print("Error fetching items:", items_data["errors"])
         return []
 
-    items = items_data['data']['boards'][0]['items_page']['items']
+    items = items_data["data"]["boards"][0]["items_page"]["items"]
     formatted_rows = []
 
     for item in items:
-        row = {'id': item['id'], 'name': item['name']}
-        for column in item['column_values']:
-            column_def = col_defs.get(column['id'], {})
-            formatted_value = format_item_value(
-                column_def.get('type', 'text'),  # Default to text if type is not found
-                column_def.get('settings_str', '{}'),
-                column['value']
-            )
-            row[column_def.get('title', column['id'])] = formatted_value  # Default to column ID if title is not found
+        row = {"id": item["id"], "name": item["name"]}
+        for column in item["column_values"]:
+            column_def = col_defs.get(column["id"], {})
+            row[column_def.get("title", column["id"])] = column["text"]
         formatted_rows.append(row)
 
     return formatted_rows
